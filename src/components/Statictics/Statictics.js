4ff34e3e3a8a5b8fbb587aaf2b55d98bfbaac9c4
mkmasudrana806 <mkmasudrana806@gmail.com>
2022-11-24 16:07:53 +0600

    rechart added

--- src/components/Statictics/Statictics.js
+++ src/components/Statictics/Statictics.js
@@ -8,36 +8,34 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
 } from "recharts";
 const Statictics = () => {
   const statisticsData = useLoaderData();
-  console.log(statisticsData);
   const { data } = statisticsData;
-  console.log(data);
   return (
-   <div className="quiz-statistics">
-     <div >
-      <h2>Quiz Statistics, topics Based</h2>
-      <AreaChart
-        width={500}
-        height={400}
-        data={data}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
+    <div className="quiz-statistics">
+      <div>
+        <h2>Quiz Statistics, topics Based</h2>
+        <AreaChart
+          className="chart-area"
+          width={450}
+          height={350}
+          data={data}
+          margin={{
+            top: 10,
+            right: 50,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="total" stroke="red" fill="#8884d8" />
+        </AreaChart>
+      </div>
     </div>
-   </div>
   );
 };
 
